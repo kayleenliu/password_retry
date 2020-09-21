590057e32a91c863271201fcbded215ea97c19aa
kayleenliu <71531568+kayleenliu@users.noreply.github.com>
2020-09-21 17:33:13 -0600

    second commit
    change to a more clear and simple syntax

--- password.py
+++ password.py
@@ -1,17 +1,15 @@
 # test password
 password = 'a123456'
 p = input ('input your password please:')
-n = 0
-while p != password and n < 3:
-    j = 2 - n
-    print('password is not correct! you have', j, 'trials left')
-    n = n + 1
-    if j > 0:
-        p = input('input your password please:')
-    elif j <= 0:
-        print('access denied')
+n = 3
+while n > 0:
+    n = n - 1
+    if p == password:
+        print('access successfully')
         break
-
-while p == password:
-    print('access successfully')
-    break
+    else:
+        if n > 0:
+            print('password is not correct! you have', n, 'trials left')
+            p = input ('input your password please:')
+        else:
+            print('access denied')       
